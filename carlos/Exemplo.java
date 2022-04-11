31d5ce73983349af8948aa876adf724d6f0544e8
cedo1011 <carloseduardodeoliveira11@gmail.com>
2022-04-11 17:52:16 +0000

    Pacotes

---
+++ carlos/Exemplo.java
@@ -0,0 +1,10 @@
+package carlos;
+
+import carlos.teste.Aluno;
+
+public class Exemplo {
+    public static void main(String[] args) {
+        Aluno al = new Aluno();
+        System.out.println("Ola Pacotes");
+    }
+}
